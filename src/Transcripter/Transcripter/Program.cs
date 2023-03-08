a175b0f5e2e7a657910c5d30ac0a351a762998e0
kostiantyn_lazurenko <kostiantyn_lazurenko@epam.com>
2023-03-08 22:03:29 +0200

    added baseline implementation

---
+++ src/Transcripter/Transcripter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+class Program
+{
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    static string speechKey = "4ce85825751042d68a60f5022c85cdd0";
+    static string speechRegion = "northeurope";
+
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+    {
+        switch (speechRecognitionResult.Reason)
+        {
+            case ResultReason.RecognizedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+
+
+        //todo for continuous recognition
+        //var speechRecognitionResult = await speechRecognizer.StartContinuousRecognitionAsync
+        OutputSpeechRecognitionResult(speechRecognitionResult);
+    }
+}
